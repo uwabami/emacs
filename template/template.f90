e428b4e69221bae8e008e4ae77148c41728355bc
Youhei SASAKI <uwabami@gfd-dennou.org>
2011-11-28 12:05:24 +0900

    add template
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

---
+++ template/template.f90
@@ -0,0 +1,19 @@
+!-*- mode: f90; coding: utf-8-unix; indent-tabs-mode: nil -*-
+!--
+!
+! Copyright(C) %author% All rights reserved.
+! $Lastupdate: $
+!
+! Author: %author% <%email%>
+! Licence:
+! This program is permitted under the principle of "NO WARRANTY" and
+! "NO RESPONSIBILITY". The author shall not be liable for any event
+! arising in any way out of the use of these resources.
+!
+! Redistribution in source and binary forms, with or without
+! modification, is also permitted provided that the above copyright
+! notice, disclaimer and this condition are retained.
+!
+!++
+
+

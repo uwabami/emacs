d7ea46e0cefd12cf7fedf646c6f6ce9c21ec6234
Youhei SASAKI <uwabami@gfd-dennou.org>
2011-12-19 15:07:39 +0900

    remove flim, semi, wl
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- site-lisp/flim/w3m-load.el
+++
@@ -1,13 +0,0 @@
-;;; w3m-load.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-(provide 'w3m-load)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; w3m-load.el ends here

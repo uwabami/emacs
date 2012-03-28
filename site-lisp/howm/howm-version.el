b1d8d36a32feac4ea3e1a0db4f48f07ec1ea38f3
Youhei SASAKI <uwabami@gfd-dennou.org>
2012-03-28 17:10:36 +0900

    Add howm
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

---
+++ site-lisp/howm/howm-version.el
@@ -0,0 +1,8 @@
+;;; howm-version.el --- Wiki-like keyword jump between files
+;;; -*- Emacs-Lisp -*-
+;;; @configure_input@
+
+(provide 'howm-version)
+(defconst howm-version "1.4.0rc2")
+
+;;; howm-version.el ends here

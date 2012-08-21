22507f1af6dfbca1da11b2e1323eab1fa274a4be
Youhei SASAKI <uwabami@gfd-dennou.org>
2012-08-22 02:03:40 +0900

    Update link
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- template/template.sh
+++ template/template.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 # -*- mode: sh; coding: utf-8-unix; indent-tabs-mode: nil -*-
 #
-# Copyright(C) %author% All rights reserved.
-# $Lastupdate: $
+# Copyright(C) %author% <%email%> All rights reserved.
+# $Lastupdate: 2012/08/21 15:15:23$
+# License: Expat
 #
-# License:
-# This program is permitted under the principle of "NO WARRANTY" and
-# "NO RESPONSIBILITY". The author shall not be liable for any event
-# arising in any way out of the use of these resources.
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
 #
-# Redistribution in source and binary forms, with or without
-# modification, is also permitted provided that the above copyright
-# notice, disclaimer and this condition are retained.
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 # Code:
-

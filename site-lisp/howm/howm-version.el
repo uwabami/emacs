5065193d395d621ed67d20bff2c853feb7d8d0be
Youhei SASAKI <uwabami@gfd-dennou.org>
2012-09-12 22:33:36 +0900

    Remove site-lisp/howm, add howm-head.rcp
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- site-lisp/howm/howm-version.el
+++
@@ -1,8 +0,0 @@
-;;; howm-version.el --- Wiki-like keyword jump between files
-;;; -*- Emacs-Lisp -*-
-;;; @configure_input@
-
-(provide 'howm-version)
-(defconst howm-version "1.4.0")
-
-;;; howm-version.el ends here

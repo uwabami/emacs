5027d37cf653ffa83669c7c5d4d3bd67855056e3
Youhei SASAKI <uwabami@gfd-dennou.org>
2014-12-28 15:37:18 +0900

    Use Cask instead of el-get
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

---
+++ Makefile
@@ -0,0 +1,46 @@
+# -*- mode: makefile -*-
+__SRC__		:= $(wildcard *.el)
+ELFiles		:= $(__SRC__:%v.el=)
+ELCFiles	:= $(ELFiles:%.el=%.elc)
+
+EMACS	?= emacs
+CASK	?= cask
+
+all: bootstrap $(ELCFiles)
+
+bootstrap: .bootstrap-stamp
+.bootstrap-stamp: .modules-stamp .cask-stamp
+.cask-stamp:
+	$(CASK) install
+	touch $@
+.modules-stamp: .permission-stamp
+	git submodule init
+	git submodule update
+	@echo "setup org-mode"
+	(cd modules/org-mode && \
+	  make compile EMACS="$(EMACS)" && \
+	  make autoloads EMACS="$(EMACS)" )
+	@echo "setup ddskkdic"
+	[ -d modules/skkdic ] || mkdir modules/skkdic
+	if [ ! -f modules/skkdic/SKK-JISYO.L ] ; then \
+	  if [ -f /usr/share/skk/SKK-JISYO.L ] ; then \
+	    ln -s /usr/share/skk/SKK-JISYO.L modules/skkdic/SKK-JISYO.L ;\
+	  else \
+	    wget -q -O - http://openlab.jp/skk/dic/SKK-JISYO.L.gz | gzip -d > modules/skkdic/SKK-JISYO.L ;\
+	  fi  \
+	fi
+	touch $@
+.permission-stamp:
+	chmod 700 tmp
+	touch $@
+%.elc: %.el
+	$(EMACS) -l init.el -batch -f batch-byte-compile $<
+conf-clean:
+	(cd config && rm -fr *.el *.elc)
+clean: conf-clean
+	rm -fr $(ELCFiles)
+distclean: clean
+	( cd modules/org-mode && $(MAKE) clean )
+	rm -fr modules/skkdic
+	rm -fr .*-stamp .*-use auto-save-list 
+recompile: clean all
---
+++ init.el
@@ -0,0 +1,178 @@
+;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil -*-
+;;
+;; Copyright(C) Youhei SASAKI All rights reserved.
+;; $Lastupdate: 2014-12-02 03:44:23$
+;;
+;; Author: Youhei SASAKI <uwabami@gfd-dennou.org>
+;; License: GPL-3+
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Comment:
+;;
+;;  org-mode に含まれる ob-tangle を改変しているので GPL-3+ で.
+;;
+;;; Code:
+;; -----------------------------------------------------------
+;;; ディレクトリ構成の決定
+;;
+;; emacs -l init.el のように起動すると load-file-name に init.el のパス
+;; が入るので, user-emacs-directory をそのディレクトリに設定する
+;;
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+(defconst my:user-emacs-share-directory
+  (expand-file-name (concat user-emacs-directory "share/")))
+(defconst my:user-emacs-config-directory
+  (expand-file-name (concat user-emacs-directory "config/")))
+(defconst my:user-emacs-temporary-directory
+  (expand-file-name (concat user-emacs-directory "tmp/")))
+(defconst my:user-emacs-package-directory
+  (expand-file-name (concat user-emacs-directory "packages/")))
+;; -----------------------------------------------------------
+;;; byte-compile 関連
+;;
+(setq debug-on-error t)
+;;
+(when (boundp 'load-prefer-newer)
+  (setq load-prefer-newer t))
+;;
+(eval-and-compile (require 'cl))
+;; Compile-Log の非表示
+(let ((win (get-buffer-window "*Compile-Log*")))
+  (when win (delete-window win)))
+;; Warning の抑制
+(setq byte-compile-warnings
+      '(not
+        free-vars
+        unresolved
+        callargs
+        redefine
+        obsolete
+        noruntime
+        cl-functions
+        interactive-only
+        make-local
+        ))
+;; -----------------------------------------------------------
+;;; load-path 追加用の関数の定義
+;;
+;; 最後に add したものが先頭にくるようになっている. 読み込みたくないファ
+;; イルは, 先頭に "." や "_" をつけると良い.
+;;
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-loadpath)
+            (normal-top-level-add-subdirs-to-load-path))))))
+;; -----------------------------------------------------------
+;;; load-path の設定
+;; load-path の優先順位が気になる場合には
+;;      M-x list-load-path-shadows
+;; で確認する.
+;;
+(add-to-load-path
+ "config"                         ; 分割した設定群の置き場所
+ "modules/cask"                   ; cask
+ "modules/org-mode/contrib/lisp"  ; org-mode
+ "modules/org-mode/lisp"          ; org-mode
+ )
+;; -----------------------------------------------------------
+;;; Handling package -- Cask, Pallet, Package.el
+;;
+(require 'cask)
+(cask-initialize)
+;;
+;;; setup use-pacakge
+;;
+(require 'use-package)
+(setq use-package-verbose t)
+;;
+;;; Pallet
+;;
+(use-package pallet
+  :init
+  (pallet-mode t))
+;;
+;; -----------------------------------------------------------
+;;; org-babel
+;; Emacs の設定はorg-mode で記述する.
+;; @see Emacsの設定ファイルをorgで書く:
+;;      http://uwabami.junkhub.org/log/20111213.html#p01
+;;
+(require 'org)
+;; -----------------------------------------------------------
+;;; ob-tangle より自分用に幾つか関数を設定
+;;
+;; my:org-babel-tangle-and-compile-file
+;; 指定された org ファイルから emacs-lisp を export してbyte-compile
+;; する. Make から呼ぶ事も想定しているのでこの段階では load はしない.
+;;
+(defun my:org-babel-tangle-and-compile-file (file)
+  "export emacs-lisp and byte-compile from org files (not load).
+   originally ob-tangle.el"
+  (interactive "fFile to load: ")
+  (flet ((age (file)
+              (float-time
+               (time-subtract (current-time)
+                              (nth 5 (or (file-attributes (file-truename file))
+                                         (file-attributes file)))))))
+    (let* ((base-name (file-name-sans-extension file))
+           (exported-file (concat base-name ".el"))
+           (compiled-file (concat base-name ".elc")))
+      ;; tangle if the org-mode file is newer than the elisp file
+      (unless (and (file-exists-p compiled-file)
+                   (> (age file) (age compiled-file)))
+        (org-babel-tangle-file file exported-file "emacs-lisp")
+        (byte-compile-file exported-file)))))
+;; -----------------------------------------------------------
+;;; my:org-babel-load-file
+;;
+;; my:org-babel-tangle-and-comile-file してから load する
+;;
+(defun my:org-babel-load-file (file)
+  "load after byte-compile"
+  (interactive "fFile to load: ")
+  (my:org-babel-tangle-and-compile-file file)
+  (load (file-name-sans-extension file)))
+;; -----------------------------------------------------------
+;;; my:org-load-file
+;;
+;; my:org-babel-load-file の際にディレクトリ名を
+;; ~/.emacs.d/config/ に決め打ち
+;;
+(defun my:load-org-file (file)
+  "my:user-emacs-config-directory 以下から my:org-babel-load-file"
+  (my:org-babel-load-file
+   (expand-file-name file my:user-emacs-config-directory)))
+;; -----------------------------------------------------------
+;;; 実際に設定を読み込む.
+;;
+(my:load-org-file "index.org")
+;; -----------------------------------------------------------
+;;; calculate bootup time/ スピード狂に捧ぐ.
+;;
+;; 目標: 3000ms 圏内 -> 最近は daemon mode で使用することが多いので意味が無い?
+;;
+(defun message-startup-time ()
+  (message
+   "Emacs loaded in %dms"
+   (/ (- (+ (third after-init-time) (* 1000000 (second after-init-time)))
+         (+ (third before-init-time) (* 1000000 (second before-init-time))))
+      1000))
+  (add-hook 'after-init-hook 'message-startup-time))
+;;; init.el ends here
